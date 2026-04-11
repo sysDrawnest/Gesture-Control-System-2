2088eaf7c4a5544ebeaa7b814c18d8fce37e461b
Sai Yangyadatta Sethy <wwwtheultimateking267@gmail.com>
2026-04-11 16:26:44 +0530

    Update __init__.py

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -2,5 +2,6 @@
 from .auth_routes import auth_bp
 from .device_routes import device_bp
 from .control_routes import control_bp
+from .canvas_routes import canvas_bp
 
-__all__ = ['auth_bp', 'device_bp', 'control_bp']
+__all__ = ['auth_bp', 'device_bp', 'control_bp', 'canvas_bp']
