1d34e8e2dd58a620aa44053e7f9701b0f5e57c25
Sai Yangyadatta Sethy <wwwtheultimateking267@gmail.com>
2026-04-17 11:12:04 +0530

    update

--- venv310/Scripts/activate.bat
+++ venv310/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Working\Final Year Project\Gesture-Control-System-2\venv310
+set VIRTUAL_ENV=C:\Users\PRATINGYA\Documents\Code\project\Gesture Control System\venv310
 
 if not defined PROMPT set PROMPT=$P$G
 
